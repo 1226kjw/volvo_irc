cbfb96a39c80cfb710fdca0abdc1a07c6b6d8344
Yeshin Lee <yeslee5004@gmail.com>
2022-03-16 01:04:48 +0900

    [Modify] irc_atoi
    1. Add overflow case
    2. Modify return value type

--- Utils.cpp
+++ Utils.cpp
@@ -43,12 +43,15 @@ bool nickcheck(std::string str)
 int irc_atoi(std::string str)
 {
 	long long int ret = 0;
+
 	for (int i = 0; str[i] && ret >= 0; ++i)
 		if (std::isdigit(str[i]))
 			ret = ret * 10 + str[i] - '0';
 		else
 			throw std::invalid_argument("");
-	if (ret < 0)
-		throw std::out_of_range("");
-	return ret;
+
+		if ((ret < 0) || (ret > 2147483647)) {
+            throw std::out_of_range("");
+	}
+	return static_cast<int>(ret);
 }
